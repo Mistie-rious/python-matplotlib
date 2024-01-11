7fc3d52c1818e310ae872c0617194d176da23df6
Mistie-rious <misturayahaya1@gmail.com>
2024-01-11 12:00:14 +0100

    first commit

--- pandas.py
+++ pandas.py
@@ -2,9 +2,12 @@ import pandas as pd
 
 data = {'x':[1,2,3,4,5], 'y':[2,4,6,8,10]}
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 
-print(df.loc[0])
+# print(df.loc[0])
 
 df = pd.read_csv('example.csv')
 print(df)
+a = df.to_numpy()
+a.reshape(4,3)
+print(a)
